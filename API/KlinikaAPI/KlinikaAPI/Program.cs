930f7bb834023cfce55b729c8dc6bfa27c71e0a4
Selma Bajramović <bajramovicselma0@gmail.com>
2024-11-27 15:05:57 +0100

    Update Program.cs

--- API/KlinikaAPI/KlinikaAPI/Program.cs
+++ API/KlinikaAPI/KlinikaAPI/Program.cs
@@ -1,10 +1,14 @@
 using KlinikaAPI.Data;
+using KlinikaAPI.Repositories.Implementation;
+using KlinikaAPI.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<KlinikaDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IDoctorRepository,DoctorRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
