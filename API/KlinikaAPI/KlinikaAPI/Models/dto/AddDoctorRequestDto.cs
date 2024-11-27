c25c4addbdf9d786424e0d357912f9e629735d4e
Selma Bajramović <bajramovicselma0@gmail.com>
2024-11-27 15:07:46 +0100

    Create DoctorDtos

---
+++ API/KlinikaAPI/KlinikaAPI/Models/dto/AddDoctorRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KlinikaAPI.Models.dto
+{
+    public class AddDoctorRequestDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string JMBG { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ API/KlinikaAPI/KlinikaAPI/Models/dto/DoctorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace KlinikaAPI.Models.dto
+{
+    public class DoctorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string JMBG { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Title { get; set; }
+        public string DoctorCode { get; set; }
+    }
+}
