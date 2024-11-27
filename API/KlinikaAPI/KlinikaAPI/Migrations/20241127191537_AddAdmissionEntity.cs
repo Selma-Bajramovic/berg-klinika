94dda1fb2b037dbda6f62a88680ef72ff158313e
Selma Bajramović <bajramovicselma0@gmail.com>
2024-11-27 20:20:39 +0100

    Admission migration

---
+++ API/KlinikaAPI/KlinikaAPI/Migrations/20241127191537_AddAdmissionEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlinikaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdmissionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdmissionDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    IsEmergency = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Admissions_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Admissions_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admissions_DoctorId",
+                table: "Admissions",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admissions_PatientId",
+                table: "Admissions",
+                column: "PatientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admissions");
+        }
+    }
+}
