21ef0b5dc5be85b80de6ccc3e0520c3d1653c5fd
Selma Bajramović <bajramovicselma0@gmail.com>
2024-11-27 15:06:43 +0100

    Update DoctorRepositories

--- API/KlinikaAPI/KlinikaAPI/Repositories/Implementation/DoctorRepository.cs
+++ API/KlinikaAPI/KlinikaAPI/Repositories/Implementation/DoctorRepository.cs
@@ -1,9 +1,46 @@
-﻿using KlinikaAPI.Repositories.Interface;
+﻿using KlinikaAPI.Data;
+using KlinikaAPI.Models.Domain;
+using KlinikaAPI.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace KlinikaAPI.Repositories.Implementation
 {
-    public class DoctorRepository:IDoctorRepository
+    public class DoctorRepository : IDoctorRepository
     {
+        private readonly KlinikaDbContext _context;
+
+        public DoctorRepository(KlinikaDbContext context)
+        {
+            _context = context;            
+        }
+
+        public async Task<IEnumerable<Doctor>> GetAllDoctorsAsync() {
+            return await _context.Doctors.ToListAsync();
+        }
+       public async Task<Doctor> GetDoctorByIdAsync(int id)
+        {
+            return await _context.Doctors.FindAsync(id);
+        }
+        public async Task AddDoctorAsync(Doctor doctor)
+        {
+            await _context.Doctors.AddAsync(doctor);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateDoctorAsync(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            await _context.SaveChangesAsync();
+
+        }
+        public async Task DeleteDoctorAsync(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor != null)
+            {
+                _context.Doctors.Remove(doctor);
+                await _context.SaveChangesAsync();
+            }
+        }
 
     }
 }
--- API/KlinikaAPI/KlinikaAPI/Repositories/Interface/IDoctorRepository.cs
+++ API/KlinikaAPI/KlinikaAPI/Repositories/Interface/IDoctorRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace KlinikaAPI.Repositories.Interface
+﻿using KlinikaAPI.Models.Domain;
+
+namespace KlinikaAPI.Repositories.Interface
 {
     public interface IDoctorRepository
     {
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<Doctor> GetDoctorByIdAsync(int id);
+        Task AddDoctorAsync(Doctor doctor);
+        Task UpdateDoctorAsync(Doctor doctor);
+        Task DeleteDoctorAsync(int id);
 
     }
 }
