5b008e703aceb4a40a60d152d884421a1a0da78d
Selma Bajramović <bajramovicselma0@gmail.com>
2024-11-28 21:25:58 +0100

    Finding migration

---
+++ API/KlinikaAPI/KlinikaAPI/Migrations/20241128201825_AddFindingEntity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlinikaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFindingEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Findings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdmissionId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Findings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Findings_Admissions_AdmissionId",
+                        column: x => x.AdmissionId,
+                        principalTable: "Admissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Findings_AdmissionId",
+                table: "Findings",
+                column: "AdmissionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Findings");
+        }
+    }
+}
